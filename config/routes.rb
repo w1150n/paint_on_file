244a1816efef1a501480dad1735027d54daa30ea
Matthew Wilson <matt@labzero.com>
2013-08-10 13:35:17 -0700

    setting the root path

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ PaintOnFile::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'locations#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
